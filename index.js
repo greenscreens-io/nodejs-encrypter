5e1905ef5211b6541baed44e0c307e31dfbb23ff
unknown <travelin.ltd@gmail.com>
2019-07-14 09:16:49 +0200

    Added APIKey and OTP support

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 
 const GSLIB = require('./lib');
 
-const SERVICE = 'http://localhost:9080';
+const SERVICE = 'http://localhost';
 
-let instance = GSLIB.initialize(SERVICE);
+// NOTE: if apiKey is used, 
+//       - OTP is not needed,
+//       - URL password will not expire,
+//       - IP address is matched to ApiKey IP
+
+const apiKey = '1a4e39c8-07d9-4a2b-b021-d1f4103d1a22';
+const otpKey = 'WWLWGFYJIF7RMXAY';
+
+let instance = GSLIB.initialize(SERVICE, apiKey, otpKey);
 
 instance.on('error', function(e) {
   console.log(e);
